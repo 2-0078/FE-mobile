6c05a1bb52a73983e92f00cb0c50d6554cc9fa40
beat1103 <beat1103@gmail.com>
2025-07-03 16:34:24 +0900

    fix: remove className prop from Search component usage

--- app/(products)/piece/page.tsx
+++ app/(products)/piece/page.tsx
@@ -105,7 +105,7 @@ export default async function PiecePage({
       <div className="px-4 mb-4">
         <div className="flex gap-2">
           <div className="flex-1 relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+            <Search />
             <Input
               placeholder="상품을 검색하세요"
               className="pl-10 bg-slate-800/50 border-gray-700 rounded-lg text-white placeholder-gray-400"
