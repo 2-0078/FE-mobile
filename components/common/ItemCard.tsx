21f7b5af762ebd1491d21394c1a871eb2d589adf
beat1103 <beat1103@gmail.com>
2025-07-04 16:48:55 +0900

    fix: add missing type prop to ItemCardImage to fix build error

--- components/common/ItemCard.tsx
+++ components/common/ItemCard.tsx
@@ -15,6 +15,7 @@ export default function ItemCard({ product }: ItemCardProps) {
         <ItemCardImage
           remainingTime="18h : 32m : 04s"
           thumbnail="https://previews.123rf.com/images/adrian1991/adrian19912109/adrian1991210900001/174101000-%EB%B3%84%EC%9D%B4-%EB%B9%9B%EB%82%98%EB%8A%94-%EB%B0%A4-%EB%B9%88%EC%84%BC%ED%8A%B8-%EB%B0%98-%EA%B3%A0%ED%9D%90-%EA%B7%B8%EB%A6%BC.jpg"
+          type="funding"
         />
         <ItemCardInfo />
       </div>
@@ -22,11 +23,13 @@ export default function ItemCard({ product }: ItemCardProps) {
   }
 
   // 실제 상품 데이터가 있을 때
-  const thumbnailImage = product.images.find(img => img.isThumbnail)?.imageUrl || product.images[0]?.imageUrl;
-  
+  const thumbnailImage =
+    product.images.find((img) => img.isThumbnail)?.imageUrl ||
+    product.images[0]?.imageUrl;
+
   // 타입 가드로 펀딩 상품과 조각 상품 구분
   const isFundingProduct = 'funding' in product;
-  
+
   if (isFundingProduct) {
     // 펀딩 상품인 경우
     const fundingProduct = product as FundingProductType;
@@ -39,8 +42,9 @@ export default function ItemCard({ product }: ItemCardProps) {
         <ItemCardImage
           remainingTime={`${remainingPieces}개 남음`}
           thumbnail={thumbnailImage}
+          type="funding"
         />
-        <ItemCardInfo 
+        <ItemCardInfo
           productName={fundingProduct.productName}
           price={fundingProduct.funding.piecePrice}
           progress={progress}
@@ -59,8 +63,9 @@ export default function ItemCard({ product }: ItemCardProps) {
         <ItemCardImage
           remainingTime={isTrading ? `${tradeQuantity}개 거래중` : '거래 종료'}
           thumbnail={thumbnailImage}
+          type="piece"
         />
-        <ItemCardInfo 
+        <ItemCardInfo
           productName={pieceProduct.productName}
           price={closingPrice || 0}
           progress={undefined}
